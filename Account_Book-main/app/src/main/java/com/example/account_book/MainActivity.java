e8e7382a0c320839913545328f6af04387a18b28
tpdks123 <132357167+tpdks123@users.noreply.github.com>
2023-05-28 18:00:09 +0900

    Add files via upload

---
+++ Account_Book-main/app/src/main/java/com/example/account_book/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.account_book;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button incomeBtn;
+    Button expenseBtn;
+    TextView incomeCategoryTextView;
+    TextView expenseCategoryTextView;
+    TextView purposeTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        // 날짜 버튼
+        Button buttonDate = findViewById(R.id.button_date);
+        buttonDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 날짜 버튼이 클릭되었을 때 실행되는 동작 구현
+                Toast.makeText(MainActivity.this, "날짜 버튼이 클릭되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // 통계 버튼
+        Button buttonStatistics = findViewById(R.id.button_statistics);
+        buttonStatistics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 통계 버튼이 클릭되었을 때 실행되는 동작 구현
+                Toast.makeText(MainActivity.this, "통계 버튼이 클릭되었습니다.", Toast.LENGTH_SHORT).show();
+
+                // StatisticsActivity로 전환하는 Intent 생성
+                Intent intent = new Intent(MainActivity.this, Expenditure_Activity.class);
+
+                // 액티비티 전환
+                startActivity(intent);
+
+
+            }
+        });
+
+        // 자산 버튼
+        Button buttonAssets = findViewById(R.id.button_assets);
+        buttonAssets.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 자산 버튼이 클릭되었을 때 실행되는 동작 구현
+                Toast.makeText(MainActivity.this, "자산 버튼이 클릭되었습니다.", Toast.LENGTH_SHORT).show();
+
+                // Data_Show_Activity로 전환하는 Intent 생성
+                Intent intent = new Intent(MainActivity.this, Data_Show_Activity.class);
+
+                // 액티비티 전환
+                startActivity(intent);
+
+            }
+        });
+
+        // MainActivity.java
+
+
+
+
+        incomeBtn = findViewById(R.id.income_btn);
+        expenseBtn = findViewById(R.id.expense_btn);
+        incomeCategoryTextView = findViewById(R.id.income_category_textview);
+        expenseCategoryTextView = findViewById(R.id.expense_category_textview);
+
+
+        incomeBtn.setOnClickListener(v -> {
+            // 카테고리 선택 다이얼로그 띄우기
+            showDialog("income");
+        });
+
+        expenseBtn.setOnClickListener(v -> {
+            // 카테고리 선택 다이얼로그 띄우기
+            showDialog("expense");
+        });
+    }
+
+    private void showDialog(String type) {
+        final String[] items;
+        if (type.equals("income")) {
+            items = getResources().getStringArray(R.array.income_categories);
+        } else { // type.equals("expense")
+            items = getResources().getStringArray(R.array.expense_categories);
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("카테고리 선택");
+        builder.setItems(items, (dialog, which) -> {
+            String category = items[which];
+            // 선택한 카테고리를 화면에 표시
+            if (type.equals("income")) {
+                incomeCategoryTextView.setText(category);
+            } else { // type.equals("expense")
+                expenseCategoryTextView.setText(category);
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+}
