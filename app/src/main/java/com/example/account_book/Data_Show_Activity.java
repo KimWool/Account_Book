c8b10796077c54640bf464066718e000d561aafe
Kim Wool <kwool0410@gmail.com>
2023-05-28 16:47:13 +0900

    [김울] 수입/지출 입력 내역 열람 기능 구현 중

--- app/src/main/java/com/example/account_book/Data_Show_Activity.java
+++ app/src/main/java/com/example/account_book/Data_Show_Activity.java
@@ -1,8 +1,12 @@
 package com.example.account_book;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.melnykov.fab.FloatingActionButton;
 
 public class Data_Show_Activity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class Data_Show_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_show);
+
+        //리스트 목록 작성
+        String[] listString = {"list 1", "list 2", "list 3", "list 4", "list 5", "list 6", "list 7", "list 8", "list 9", "list 10"};
+
+        //리스트 어뎁터 생성
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listString);
+
+        //리스트 뷰에 어뎁터 적용
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(adapter);
+
+        //Floating Action Button을 리스트 뷰에 적용
+        FloatingActionButton fab = findViewById(R.id.add_button);
+        fab.attachToListView(listView);
+
+        //이벤트 적용
+        fab.setOnClickListener(v -> {
+            //여기에 버튼 클릭 시 이벤트 작성
+        });
     }
 }
