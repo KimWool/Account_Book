9ecadbaf6bbbd42d66c6c4e6c6dc3febd3052dd7
Kim Wool <kwool0410@gmail.com>
2023-05-29 20:26:27 +0900

    [김울] 수입/지출 데이터 추가 기능 구현

--- app/src/main/java/com/example/account_book/Sub_Expense_Activity.java
+++ app/src/main/java/com/example/account_book/Sub_Expense_Activity.java
@@ -1,15 +1,24 @@
 package com.example.account_book;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Sub_Expense_Activity extends AppCompatActivity {
+
+    EditText amount_input;
+    EditText content_input;
 
     @SuppressLint("UseSwitchCompatOrMaterialCode")
     Switch aSwitch;
@@ -23,6 +32,11 @@ public class Sub_Expense_Activity extends AppCompatActivity {
 
         Spinner expense_spinner = findViewById(R.id.expense_spinner);
 
+        amount_input = findViewById(R.id.amount_input);
+        content_input = findViewById(R.id.content_input);
+
+        Button insert_button = findViewById(R.id.insert_button);
+
         expense_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -35,6 +49,30 @@ public class Sub_Expense_Activity extends AppCompatActivity {
             }
         });
 
-        //스위치 클릭 이벤트 추가하기
+        insert_button.setOnClickListener(v -> {
+            String strExpense = expense_spinner.getSelectedItem().toString();
+            String strAmountText = amount_input.getText().toString();
+            Integer strAmount = Integer.parseInt(strAmountText);
+            String strContent = content_input.getText().toString();
+
+            boolean switchState = aSwitch.isChecked();
+
+            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Data");
+            String key = databaseReference.push().getKey();
+            assert key != null;
+            DatabaseReference newDataRef = databaseReference.child(key);
+            newDataRef.child("category").setValue(strExpense);
+            newDataRef.child("amount").setValue(strAmount);
+            newDataRef.child("content").setValue(strContent);
+
+            if (switchState) {
+                newDataRef.child("fixed_data").setValue("고정 지출");
+            } else {
+                newDataRef.child("fixed_data").setValue("변동 지출");
+            }
+
+            Intent intent = new Intent(Sub_Expense_Activity.this, Data_Show_Activity.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/example/account_book/Sub_Income_Activity.java
+++ app/src/main/java/com/example/account_book/Sub_Income_Activity.java
@@ -1,15 +1,24 @@
 package com.example.account_book;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Sub_Income_Activity extends AppCompatActivity {
+
+    EditText amount_input;
+    EditText content_input;
 
     @SuppressLint("UseSwitchCompatOrMaterialCode")
     Switch aSwitch;
@@ -23,6 +32,11 @@ public class Sub_Income_Activity extends AppCompatActivity {
 
         Spinner income_spinner = findViewById(R.id.income_spinner);
 
+        amount_input = findViewById(R.id.amount_input);
+        content_input = findViewById(R.id.content_input);
+
+        Button insert_button = findViewById(R.id.insert_button);
+
         income_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -34,6 +48,34 @@ public class Sub_Income_Activity extends AppCompatActivity {
 
             }
         });
-        //스위치 클릭 이벤트 추가하기
+
+        insert_button.setOnClickListener(v -> {
+            String strIncome = income_spinner.getSelectedItem().toString();
+            String strAmountText = amount_input.getText().toString();
+            Integer strAmount = Integer.parseInt(strAmountText);
+            String strContent = content_input.getText().toString();
+
+            boolean switchState = aSwitch.isChecked();
+
+            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Data");
+            String key = databaseReference.push().getKey();
+            assert key != null;
+            DatabaseReference newDataRef = databaseReference.child(key);
+            newDataRef.child("category").setValue(strIncome);
+            newDataRef.child("amount").setValue(strAmount);
+            newDataRef.child("content").setValue(strContent);
+
+            if (switchState) {
+                // Switch is ON, perform action A
+                newDataRef.child("fixed_data").setValue("고정 수입");
+            } else {
+                // Switch is OFF, perform action B
+                newDataRef.child("fixed_data").setValue("변동 수입");
+            }
+
+            Intent intent = new Intent(Sub_Income_Activity.this, Data_Show_Activity.class);
+            startActivity(intent);
+        });
+
     }
 }
