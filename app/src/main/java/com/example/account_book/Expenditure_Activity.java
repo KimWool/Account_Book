3a7f2f48c5ba19337d09003db3c7f67cfab3505b
kimsk79 <kdasmekl@gmail.com>
2023-05-30 01:44:50 +0900

    파이어베이스 연동 및 막대 차트 수정

--- app/src/main/java/com/example/account_book/Expenditure_Activity.java
+++ app/src/main/java/com/example/account_book/Expenditure_Activity.java
@@ -1,179 +1,208 @@
 package com.example.account_book;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Locale;
-
-import android.graphics.Color;
 import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 
 public class Expenditure_Activity extends AppCompatActivity {
-
     private TextView monthTextView;
     private TextView expensesTextView;
     private TextView incomeTextView;
-
-    private Calendar calendar;
-    private DateFormat monthFormat;
-
     private BarChart barChart;
+    private Calendar calendar;
+
+    private List<Integer> expensesList;
+    private List<Integer> incomeList;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expenditure);
 
-        // XML 레이아웃 파일에서 해당 뷰들을 가져옴
+        // Firebase 초기화
+        FirebaseApp.initializeApp(this);
+
+        // XML에서 해당하는 뷰들을 찾아와 변수에 할당
         monthTextView = findViewById(R.id.monthTextView);
+        Button previousButton = findViewById(R.id.previousButton);
+        Button nextButton = findViewById(R.id.nextButton);
         expensesTextView = findViewById(R.id.expensesTextView);
         incomeTextView = findViewById(R.id.incomeTextView);
-
-        // 달력 및 월 형식 설정을 위한 변수 초기화
-        calendar = Calendar.getInstance();
-        monthFormat = new SimpleDateFormat("MMMM", Locale.getDefault());
-
-        // 막대 차트 초기화
         barChart = findViewById(R.id.bar_chart);
-        setupBarChart();
 
-        // 초기화된 월, 소비 및 수입 데이터를 화면에 업데이트함
-        updateMonth();
-        updateExpensesForMonth();
-        updateIncomeForMonth();
+        // Calendar 객체를 생성하고 현재 날짜로 설정
+        calendar = Calendar.getInstance();
 
-        // 이전 달 버튼 클릭 시 이벤트 처리
-        Button previousButton = findViewById(R.id.previousButton);
+        // 초기에 현재 달을 표시
+        updateMonthText();
+
+        // previousButton 클릭 시 이전 달로 변경
         previousButton.setOnClickListener(v -> {
             calendar.add(Calendar.MONTH, -1);
-            updateMonth();
-            updateExpensesForMonth();
-            updateIncomeForMonth();
+            updateMonthText();
+            fetchDataFromFirebase();
         });
 
-        // 다음 달 버튼 클릭 시 이벤트 처리
-        Button nextButton = findViewById(R.id.nextButton);
+        // nextButton 클릭 시 다음 달로 변경
         nextButton.setOnClickListener(v -> {
             calendar.add(Calendar.MONTH, 1);
-            updateMonth();
-            updateExpensesForMonth();
-            updateIncomeForMonth();
+            updateMonthText();
+            fetchDataFromFirebase();
         });
 
-        // 월별 소비 총액 데이터 (예시)
-        int[] monthlyExpenses = new int[12]; // 12개월에 대한 데이터를 담을 배열
+        // BarChart 데이터 설정
+        setupBarChart();
 
-        // 월별 소비 데이터 초기화
-        for (int i = 0; i < monthlyExpenses.length; i++) {
-            monthlyExpenses[i] = getExpensesForMonth(i); // 각 월에 대한 소비값을 메서드로부터 가져옴
-        }
-
-        ArrayList<BarEntry> entries = new ArrayList<>();
-        for (int i = 0; i < monthlyExpenses.length; i++) {
-            entries.add(new BarEntry(i, monthlyExpenses[i]));
-        }
-
-        // 막대 데이터셋 생성
-        BarDataSet dataSet = new BarDataSet(entries, "월별 소비 총액");
-        dataSet.setColor(Color.GRAY); // 막대의 색상 (회색)
-        dataSet.setDrawValues(true); // 막대 위에 값 표시 활성화
-
-        BarData barData = new BarData(dataSet);
-        barChart.setData(barData);
-
-        XAxis xAxis = barChart.getXAxis();
-        xAxis.setValueFormatter(new ValueFormatter() {
-            @Override
-            public String getFormattedValue(float value) {
-                int monthIndex = (int) value;
-                Calendar monthCalendar = Calendar.getInstance();
-                monthCalendar.setTime(calendar.getTime());
-                monthCalendar.add(Calendar.MONTH, monthIndex);
-                return monthFormat.format(monthCalendar.getTime());
-            }
-        });
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setGranularity(1f);
-        xAxis.setLabelCount(monthlyExpenses.length); // 보여줄 레이블 수
-        barChart.setDragEnabled(true);
-        barChart.setVisibleXRangeMaximum(6); // 한 번에 보여줄 최대 레이블 수
-
-        // y축 설정
-        YAxis yAxisLeft = barChart.getAxisLeft();
-        yAxisLeft.setGranularity(1f); // 간격 설정
-        yAxisLeft.setAxisMinimum(0f); // 최소 값 설정
-        // 월별 소비 값 중 최댓값을 구하여 최댓값보다 약간 더 큰 값으로 최대 값 설정
-        int maxExpense = getMaxExpense(monthlyExpenses);
-        yAxisLeft.setAxisMaximum(maxExpense + 100); // 최대 값 설정
-
-        YAxis yAxisRight = barChart.getAxisRight();
-        yAxisRight.setEnabled(false); // 오른쪽 y축 비활성화
-
-        barChart.invalidate();
-
-        // TODO: 해당 달의 소비와 수입 데이터를 토대로 expensesTextView, incomeTextView 업데이트
+        // Firebase에서 데이터 가져오기
+        fetchDataFromFirebase();
     }
 
-    // 해당 달의 소비값을 업데이트하는 메서드
-    private void updateExpensesForMonth() {
-        int expenses = getExpensesForMonth(calendar.get(Calendar.MONTH));
-        expensesTextView.setText(String.valueOf(expenses));
-    }
-
-    // 해당 달의 수입값을 업데이트하는 메서드
-    private void updateIncomeForMonth() {
-        int income = getIncomeForMonth(calendar.get(Calendar.MONTH));
-        incomeTextView.setText(String.valueOf(income));
-    }
-
-    // 현재 선택된 달을 업데이트하는 메서드
-    private void updateMonth() {
-        String monthString = monthFormat.format(calendar.getTime());
+    // monthTextView에 현재 달을 표시하는 메서드
+    private void updateMonthText() {
+        SimpleDateFormat sdf = new SimpleDateFormat("MMMM", Locale.getDefault());
+        String monthString = sdf.format(calendar.getTime());
         monthTextView.setText(monthString);
     }
 
-    // TODO: 해당 달의 소비값을 반영하는 메서드 구현
-    private int getExpensesForMonth(int month) {
-        return 1000; // 임시로 1000으로 설정
+    // BarChart 데이터 설정 메서드
+    private void setupBarChart() {
+        List<BarEntry> entries = new ArrayList<>();
+
+        // BarDataSet 생성 및 속성 설정
+        BarDataSet dataSet = new BarDataSet(entries, "금액");
+        dataSet.setColors(new int[]{R.color.expenses_color, R.color.income_color}, this);
+
+        // BarData 생성 및 dataSet 설정
+        BarData barData = new BarData(dataSet);
+        barData.setBarWidth(0.5f);
+
+        // BarChart 속성 설정
+        barChart.setData(barData);
+        barChart.setFitBars(true);
+        barChart.setDrawValueAboveBar(true);
+        barChart.getDescription().setEnabled(false);  // 그래프 설명 표시 비활성화
+
+        // x축 설정
+        barChart.getXAxis().setGranularity(1f);
+        barChart.getXAxis().setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+                // x축 레이블 설정
+                if (value == 0)
+                    return "지출";
+                else if (value == 1)
+                    return "수입";
+                else
+                    return "";
+            }
+        });
+
+        // y축 설정
+        barChart.getAxisLeft().setAxisMinimum(0f); // y축의 최소값을 0으로 설정
+        barChart.getAxisRight().setEnabled(false);  // 오른쪽 y축 비활성화
+
+        // BarChart 갱신
+        barChart.invalidate();
     }
 
-    // TODO: 해당 달의 수입값을 반영하는 메서드 구현
-    private int getIncomeForMonth(int month) {
-        return 2000; // 임시로 2000으로 설정
+    // Firebase에서 데이터 가져오기
+    private void fetchDataFromFirebase() {
+        String databaseUrl = "https://account-book-889ee-default-rtdb.firebaseio.com/";
+        String dataPath = "Data";
+
+        FirebaseDatabase.getInstance(databaseUrl)
+                .getReference(dataPath)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    // 수입과 지출에 대한 데이터를 저장하는 리스트
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        expensesList = new ArrayList<>();
+                        incomeList = new ArrayList<>();
+
+                        // fixed_data와 amount의 속성 값 가져옴
+                        for (DataSnapshot data : dataSnapshot.getChildren()) {
+                            String fixedData = data.child("fixed_data").getValue(String.class);
+                            Integer amountInt = data.child("amount").getValue(Integer.class);
+
+                            // amountInt가 null이 아닌 경우에만 처리 진행
+                            if (amountInt != null) {
+                                int amount = amountInt;
+
+                                // fixedData가 null이 아니고 "지출"을 포함하는 경우 expensesList에 금액을 추가
+                                // "수입"을 포함하는 경우에는 incomeList에 금액을 추가
+                                if (fixedData != null && fixedData.contains("지출")) {
+                                    expensesList.add(amount);
+                                } else if (fixedData != null && fixedData.contains("수입")) {
+                                    incomeList.add(amount);
+                                }
+                            }
+                        }
+
+                        // expensesList와 incomeList의 총합 계산
+                        int expensesTotal = calculateTotal(expensesList);
+                        int incomeTotal = calculateTotal(incomeList);
+
+                        // expensesTextView와 incomeTextView에 금액 표시
+                        expensesTextView.setText(String.format(Locale.getDefault(), "%7d", expensesTotal));
+                        incomeTextView.setText(String.format(Locale.getDefault(), "%7d", incomeTotal));
+
+                        // BarChart에 데이터 추가
+                        List<BarEntry> entries = new ArrayList<>();
+                        entries.add(new BarEntry(0, expensesTotal));
+                        entries.add(new BarEntry(1, incomeTotal));
+
+                        // BarDataSet 생성 및 속성 설정
+                        BarDataSet dataSet = new BarDataSet(entries, "금액");
+                        dataSet.setColors(new int[]{R.color.expenses_color, R.color.income_color}, Expenditure_Activity.this);
+                        dataSet.setValueTextSize(12f);  // 숫자 크기 설정
+
+                        // BarData 생성 및 dataSet 설정
+                        BarData barData = new BarData(dataSet);
+                        barData.setBarWidth(0.5f);
+
+                        // BarChart에 데이터 설정
+                        barChart.setData(barData);
+                        barChart.notifyDataSetChanged();
+                        barChart.invalidate();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        // 데이터 읽기가 실패한 경우 호출되는 콜백 메서드
+                        Log.d("Firebase", "Failed to read data: " + databaseError.getMessage());
+                    }
+                });
     }
 
-    // 최댓값을 구하는 메서드
-    private int getMaxExpense(int[] expenses) {
-        int max = Integer.MIN_VALUE;
-        for (int expense : expenses) {
-            if (expense > max) {
-                max = expense;
+    // 리스트의 총합을 계산하는 메서드
+    private int calculateTotal(List<Integer> dataList) {
+        int total = 0;
+        if (dataList != null) {
+            for (int value : dataList) {
+                total += value;
             }
         }
-        return max;
-    }
-
-    // 막대 차트 초기 설정 메서드
-    private void setupBarChart() {
-        barChart.setDrawBarShadow(false);
-        barChart.setDrawValueAboveBar(true);
-        barChart.getDescription().setEnabled(false);
-        barChart.getLegend().setEnabled(false);
-        barChart.setPinchZoom(true);
-        barChart.setScaleEnabled(false);
-        barChart.setDragEnabled(true);
+        return total;
     }
 }
